f25bf492f0573512ee445146618233074d406db1
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-18 01:36:10 +0200

    Fixed a typo in FE file to make docker work

--- src/components/Explore/TotExplore.js
+++ src/components/Explore/TotExplore.js
@@ -6,7 +6,7 @@ import Slideshow from "./components/photostream/Slideshow"*/
 import React from 'react';
 import Explorenav from './Explorenav'
 import Exploresub_nav from './Exploresub_nav'
-import Pictures from "./Pictures"
+import Pictures from "./pictures"
 
 function TotExplore() {
  
