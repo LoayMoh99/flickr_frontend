d4651e3e22f913972b87be2d74e735214f952275
samarnabil <samarnabil22@gmail.com>
2021-05-24 22:34:09 +0200

    post new comment feature

--- src/components/photostream/CommentBox.jsx
+++ src/components/photostream/CommentBox.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import Comment from "./Comment"
 import './CommentBox.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faComment} from '@fortawesome/free-solid-svg-icons'
@@ -6,13 +7,36 @@ import {faComment} from '@fortawesome/free-solid-svg-icons'
 function CommentBox(props){
     const comment = <FontAwesomeIcon icon={faComment} color="white"/>
 
+    const [newComment, setComment] = useState('')
+    const [placeholderText , setText] = useState("Add a comment about this photo")
+
+    function handleComment(event){
+        const toComment = event.target.value;
+        setComment(toComment);
+    }
+
+    function postComment(){
+        console.log(newComment);
+        setText("Add a comment about this photo");
+        //API
+        document.getElementById('com').value = '';
+    }
+
     return(
         <>
             <div className="comment-box">
-                <div className="comments"></div><hr/>
+                <div className="comments">
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>  
+                </div><hr/>
                 <div className="mycomment">
-                    <textarea className="form-control" rows="2" placeholder="Add a comment about this photo"/>
-                    <button>Add comment</button>
+                    <textarea id="com" className="form-control" rows="2" placeholder={placeholderText} onChange={handleComment}/>
+                    <button onClick={postComment}>Add comment</button>
                 </div>
             </div>
         </>
