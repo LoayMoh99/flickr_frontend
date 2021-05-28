effe54d29ed170b27f82c7bf32a12042541490d5
farahmostafa11 <58088212+farahmostafa11@users.noreply.github.com>
2021-05-28 17:44:04 +0200

    Upload Photo with Validations

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -3,18 +3,34 @@ import '../../fonts/font/flaticon.css'
 import './Upload.css'
 import flickrPhoto from '../../img/flickr.jpg'
 import { Link } from 'react-router-dom'
+import { event } from 'jquery'
 
 export default function Upload(props){
-    const imageUpload = (e) => {
-        const file = e.target.files[0];
-        getBase64(file).then(base64 => {
-            localStorage["fileBase64"] = base64;
-            console.debug("file stored",base64);
-        });
-    };
+    
+    // const getBase64 = (file) => {
+    //     return new Promise((resolve,reject) => {
+    //         const reader = new FileReader();
+    //         reader.onload = () => resolve(reader.result);
+    //         reader.onerror = error => reject(error);
+    //         reader.readAsDataURL(file);
+    // });}
+    // const imageUpload = (e) => {
+    //     const file = e.target.files[0];
+    //     getBase64(file).then(base64 => {
+    //         localStorage["fileBase64"] = base64;
+    //         console.debug("file stored",base64);
+    //     });
+    // };
     const navStyle={
         color:'white'
     };
+    //const [isPhotoSelected,setPhotoSelected] = useState(false);
+
+    const [image, setImage] = useState();
+    const onchange = e => setImage(URL.createObjectURL(e.target.files[0]));
+
+    
+
     return(
         <div className="uploadNavbar">
             <nav className="navbar fix_nav">
@@ -31,22 +47,19 @@ export default function Upload(props){
             </div>
             </div>
             </nav>
+
             <div className="uploadText">
                 <h3>You can upload 1000 more photos and videos</h3>
                 <p>Drag & drop photos here</p>
                 <p>or</p>
                 <div>
-                <input type="file"  id="imageFile" name='imageFile'  onChange={imageUpload}/>
+                <p><input type="file"  accept="image/*" name="image" id="file"  onChange={onchange}/></p>
+                <p><label for="file" >Upload Image</label></p>
+                <p><img id="output" width="200" /></p>
+                {image && <img src={image} alt="The current file" id="selectedImg" />}
                 </div>
             </div>
         </div>
     );
-    const getBase64 = (file) => {
-        return new Promise((resolve,reject) => {
-            const reader = new FileReader();
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = error => reject(error);
-            reader.readAsDataURL(file);
-        });}
     
 }
